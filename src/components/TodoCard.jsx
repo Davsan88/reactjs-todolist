66b3fa2bdea42b0703901a73cad25c4ddc814018
DavSan <david.sanchez.martinez@outlook.com>
2024-05-06 17:27:28 +0100

    Update TodoCard.jsx

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function TodoCard() {
   return (
-    <div>TodoCard</div>
+    <li className='todoItem'>
+      <div className='actionsContainer'>
+        <i className="fa-solid fa-pen-to-square"></i>
+        <i className="fa-solid fa-trash"></i>
+      </div>
+    </li>
   )
 }
