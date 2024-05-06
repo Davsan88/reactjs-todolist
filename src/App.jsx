1f6d82a02452604f4ba00c1fc28c0008a896bfaf
DavSan <david.sanchez.martinez@outlook.com>
2024-05-06 11:07:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import TodoInput from "./components/TodoInput"
+import TodoInput from "./components/TodoList"
+
 function App() {
   
   return (
-    <>
-      
-    </>
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
   )
 }
 
