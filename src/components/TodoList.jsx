6bd735ac741becf054fb4128db72338430d77b81
DavSan <david.sanchez.martinez@outlook.com>
2024-05-07 19:39:45 +0100

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import TodoCard from './TodoCard'
 
 export default function TodoList() {
-  let todos = [
-    'Go to the gym',
-    'Take the dogs to the park',
-    'Learn how to built amazing apps'
-  ]
+  
 
-  return (s
+  return (
     <ul className='main'>
       {todos.map((todo, todoIndex) => {
         return (
