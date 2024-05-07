ba06ee3596d53284d54985d7d59c20769c4b9c2c
DavSan <david.sanchez.martinez@outlook.com>
2024-05-07 19:51:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = useState([
     'Go to the gym',
     'Take the dogs to the park',
     'Learn how to built amazing apps'
-  ]
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
 
   return (
     <>
