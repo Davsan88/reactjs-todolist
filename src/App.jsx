bbb8755fe751807fa7fe76a4b61161b1d1eecc59
DavSan <david.sanchez.martinez@outlook.com>
2024-05-07 19:39:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
+  let todos = [
+    'Go to the gym',
+    'Take the dogs to the park',
+    'Learn how to built amazing apps'
+  ]
+
   return (
     <>
       <TodoInput />
-      <TodoList />
+      <TodoList todos={todos} />
     </>
   )
 }
