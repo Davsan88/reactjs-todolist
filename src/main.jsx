584641e011b7fcdbbf38a2b1c2bbd7ac887dbf21
DavSan <david.sanchez.martinez@outlook.com>
2024-08-16 20:05:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'                   // Importing the React library
+import ReactDOM from 'react-dom/client'     // Importing ReactDOM for rendering the React app
+import App from './App.jsx'                 // Importing the main App component
+import './index.css'                        // Importing the CSS file for styling
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
