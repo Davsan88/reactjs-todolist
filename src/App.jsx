704ed622440b1077838260f73db054df7d1b459b
DavSan <david.sanchez.martinez@outlook.com>
2024-08-16 19:52:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import TodoInput from "./components/TodoInput" // Importing the TodoInput compon
 import TodoList from "./components/TodoList" // Importing the TodoList component
 
 function App() {
-  const [todos, setTodos] = useState([])
+  // Declaring a state variable 'todos' to store the list of todos and a setter function 'setTodos' to update it
+  const [todos, setTodos] = useState([]) 
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
