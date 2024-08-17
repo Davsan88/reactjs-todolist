228e6d1556f40eaa8563b6f7825939c6ebb0e2ec
DavSan <david.sanchez.martinez@outlook.com>
2024-08-17 17:34:35 +0100

    Update TodoInput.jsx

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"  // Importing useState from React to manage sta
 export default function TodoInput(props) {
     // Destructuring the handleAddTodos function from the props passed to this component
     const {handleAddTodos} = props
+    // Declaring state to store the current value of the todo input
     const [todoValue, setTodoValue] = useState('')
 
     return (
